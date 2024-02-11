c6c812bf1e20b339ec50e2fd9d9d3da3766cdcdb
Lisa-Centracorn <alisa.dammer@gmail.com>
2024-02-11 21:44:34 +0100

    added dropdown for endpoints in config dialog

--- lib/widgets/config_dialog.dart
+++ lib/widgets/config_dialog.dart
@@ -14,16 +14,66 @@ class ConfigDialogState extends State<ConfigDialog> {
   SharedPreferences get prefs => widget.prefs;
   late UserPreference userConf;
 
+  String? _endpoints;
+
   @override
   void initState() {
     super.initState();
     userConf = UserPreference().getPreference(prefs);
+    _endpoints = endpointsRevers[userConf.endpointToUse];
+  }
+
+  Map<String, int> endpointsDrop = {"All": 2, "Closed": 0, "OpenAI": 1};
+  Map<int, String> endpointsRevers = {2: "All", 0: "Closed", 1: "OpenAI"};
+
+  List<DropdownMenuItem<String>> getDDItems() {
+    List<DropdownMenuItem<String>> res = [];
+    res.addAll(endpointsDrop.keys
+        .toList()
+        .map<DropdownMenuItem<String>>((String value) {
+      return DropdownMenuItem<String>(value: value, child: Text(value));
+    }).toList());
+    return res;
   }
 
   @override
   Widget build(BuildContext context) {
-    print("${userConf.areaBlacklist}");
-    return const Dialog(
-        child: Column(children: [Text("Here comes configuration")]));
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    return Dialog(
+        child: SingleChildScrollView(
+            child: SizedBox(
+                width: width * 0.9,
+                height: height * 0.8,
+                child: Column(children: [
+                  const Text("Configuration"),
+                  Row(children: [
+                    const Expanded(
+                        child: Text("Endpoits", textAlign: TextAlign.center)),
+                    Expanded(
+                        child: DropdownButton<String>(
+                            focusColor: Colors.white,
+                            value: _endpoints,
+                            items: getDDItems(),
+                            onChanged: (String? value) {
+                              setState(() {
+                                _endpoints = value!;
+                              });
+                            },
+                            hint: const Text('Text gen')))
+                  ]),
+                  // Row(),
+                  // Row(),
+                  TextButton(
+                    child: const Text("Save"),
+                    onPressed: () {
+                      setState(() {
+                        userConf.setPreference(
+                            prefs, endpointsDrop[_endpoints], null, null, null);
+                      });
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ]))));
   }
 }
