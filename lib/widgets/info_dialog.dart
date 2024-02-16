88bdc7388cb01c52219868fa05d9cf200865c3b3
Lisa-Centracorn <alisa.dammer@gmail.com>
2024-02-16 13:01:48 +0100

    setup info page

--- lib/widgets/info_dialog.dart
+++ lib/widgets/info_dialog.dart
@@ -15,7 +15,27 @@ class InfoDialogState extends State<InfoDialog> {
 
   @override
   Widget build(BuildContext context) {
-    return const Dialog(
-        child: Column(children: [Text("Here comes disclaimer and links")]));
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    return Dialog(
+        child: Container(
+            height: height * 0.9,
+            width: width * 0.9,
+            child: CustomScrollView(slivers: <Widget>[
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    return const Padding(
+                        padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                        child: Center(
+                          child: Text(
+                              "1. Not a Mental Health App: \nPosicorn is not a mental health app. It is designed for lifestyle management and is not a substitute for professional mental health care."),
+                        ));
+                  },
+                  childCount: 1,
+                ),
+              ),
+            ])));
+    // child: Column(children: [Text("Here comes disclaimer and links")]));
   }
 }
