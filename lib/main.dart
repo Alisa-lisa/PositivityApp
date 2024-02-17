7990f9c9bcb7177bd740701a376cacc1d6c00499
Lisa-Centracorn <alisa.dammer@gmail.com>
2024-02-17 19:30:07 +0100

    add text boxes for answers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:positivityapp/models/configuration.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
@@ -71,10 +72,12 @@ class _MyHomePageState extends State<MyHomePage> {
   String get deviceId => widget.deviceId;
   List<String> tags = ["social", "family", "romantic", "health", "career"];
   List<String> difficulty = ["simple", "neutral", "hard"];
+  late UserPreference userConf;
 
   @override
   void initState() {
     super.initState();
+    userConf = UserPreference().getPreference(prefs);
   }
 
   Future<String> _getText() async {
@@ -101,19 +104,49 @@ class _MyHomePageState extends State<MyHomePage> {
         body: FutureBuilder(
             future: _getText(),
             builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Center(
-                      child: Container(
-                    height: height * 0.3,
-                    width: width * 0.9,
-                    color: Colors.blue[50],
-                    child: Text(snapshot.data.toString(),
-                        style: const TextStyle(fontSize: 18)),
-                  )),
-                ],
-              );
+              return CustomScrollView(slivers: [
+                SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                        (BuildContext context, int index) {
+                  return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Center(
+                            child: Text("Life Scenario:",
+                                style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold))),
+                        Center(
+                            child: Container(
+                          height: height * 0.3,
+                          width: width * 0.9,
+                          color: Colors.blue[50],
+                          child: Text(snapshot.data.toString(),
+                              style: const TextStyle(fontSize: 18)),
+                        )),
+                      ]);
+                }, childCount: 1)),
+                SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                        (BuildContext context, int index) {
+                  return const Center(
+                      child: Text("Optimistic views:",
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold)));
+                }, childCount: 1)),
+                SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                        (BuildContext context, int index) {
+                  return const Padding(
+                      padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      child: TextField(
+                          obscureText: false,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Optimistic/Positive outlook',
+                          )));
+                }, childCount: userConf.minimumPositive))
+              ]);
             }),
         floatingActionButtonLocation:
             FloatingActionButtonLocation.miniCenterFloat,
